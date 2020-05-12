3dd071f3bd5bf2c42d5b1a588fd5371d60465d53
yambo92 <1810962886@qq.com>
2020-05-12 23:38:20 +0800

    'event'

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,71 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Confirm from './Confirm'
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and TypeScript
-        </a>
-      </header>
-      <Confirm
-        title="React and TypeScript"
-        content="Are you sure you want to learn TS"
-        cancelCaption="不可能"
-        okCaption="好好好"
-      />
-    </div>
-  )
+
+interface IState {
+  confirmOpen: boolean
+  confirmMessage: string
+}
+/* 
+ tell the  App component about the state type, which we can do using the 
+second generic parameter of  React.Component;
+We have used  {} as the props type because there are no props for this
+component
+*/
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      confirmOpen: true,
+      confirmMessage: 'Please hit the confirm button',
+    }
+  }
+  private handleCancelConfirmClick = () => {
+    this.setState({
+      confirmOpen: false,
+      confirmMessage: "Take a break, i'm sure you will later...",
+    })
+  }
+  private handleOkConfirmClick = () => {
+    this.setState({
+      confirmOpen: false,
+      confirmMessage: 'Cool, carry on reading!',
+    })
+  }
+  private handleConfirmClick = () => {
+    this.setState({ confirmOpen: true })
+  }
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React and TypeScript
+          </a>
+        </header>
+        <p>{this.state.confirmMessage}</p>
+        <button onClick={this.handleConfirmClick}>Confirm</button>
+        <Confirm
+          open={this.state.confirmOpen}
+          title="React and TypeScript"
+          content="Are you sure you want to learn TS"
+          cancelCaption="不可能"
+          okCaption="好好好"
+          onOkClick={this.handleOkConfirmClick}
+          onCancelClick={this.handleCancelConfirmClick}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/Confirm.tsx
+++ src/Confirm.tsx
@@ -3,10 +3,13 @@ import * as React from 'react'
 import './Confirm.css'
 
 interface IProps {
+  open: boolean
   title: string
   content: string
   cancelCaption?: string
   okCaption?: string
+  onOkClick: () => void
+  onCancelClick: () => void
 }
 
 class Confirm extends React.Component<IProps> {
@@ -15,9 +18,17 @@ class Confirm extends React.Component<IProps> {
     okCaption: 'Okay',
   }
 
+  private handleOkClick = () => {
+    this.props.onOkClick()
+  }
+
+  private handleCancelClick = () => {
+    this.props.onCancelClick()
+  }
+
   public render() {
     return (
-      <div className="confirm-wrapper confirm-visible">
+      <div className={this.props.open ? 'confirm-wrapper confirm-visible' : 'confirm-wrapper'}>
         <div className="confirm-container">
           <div className="confirm-title-container">
             <span>{this.props.title}</span>
@@ -26,8 +37,12 @@ class Confirm extends React.Component<IProps> {
             <p>{this.props.content} </p>
           </div>
           <div className="confirm-buttons-container">
-            <button className="confirm-cancel">{this.props.cancelCaption}</button>
-            <button className="confirm-ok">{this.props.okCaption}</button>
+            <button className="confirm-cancel" onClick={this.handleCancelClick}>
+              {this.props.cancelCaption}
+            </button>
+            <button className="confirm-ok" onClick={this.handleOkClick}>
+              {this.props.okCaption}
+            </button>
           </div>
         </div>
       </div>
